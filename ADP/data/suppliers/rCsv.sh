02a904f044d24921fc52da15946b6305eaa9c397
shaunmsoria <shaunmsoria@gmail.com>
2022-10-24 23:08:14 +0000

    Organising Repo

--- ADP/data/suppliers/rCsv.sh
+++
@@ -1,12 +0,0 @@
-#!/bin/bash
-
-printf "{" >> tmp.json
-
-while IFS="," read -r SKU Des Supplier
-do
-	printf "\"%s\":\n\t{\n\t\"Description\":\"%s\",\n\t\"Supplier\":\"%s\"\n\t},\n" "$SKU" "$Des" "$Supplier" >> tmp.json
-done < <(tail -n +2 itemSup.csv)
-
-sed '$ s/.$/}/' tmp.json > itemSup.json
-
-rm -f tmp.json temp.json
--- ADP/data/suppliers/rCsv2.sh
+++
@@ -1,12 +0,0 @@
-#!/bin/bash
-
-printf "{" >> tmp.json
-
-while IFS="," read -r SKU Des Supplier
-do
-	printf "\"%s\":{\"Description\":\"%s\",\"Supplier\":\"%s\"}," "$SKU" "$Des" "$Supplier" >> tmp.json
-done < <(tail -n +2 itemSup.csv)
-
-sed '$ s/.$/}/' tmp.json > itemSup.json
-
-rm -f tmp.json temp.json
