068e9f171af5911bf194af719f99990e0c5ce8ad
shaunmsoria <shaunmsoria@gmail.com>
2022-10-25 22:45:59 +0000

    Improvement on the init.vim file

---
+++ OKR/ITokr/DB/itemDB.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+database=$1
+
+rm -f tmp.json tmp2.json
+
+# jq '.' $1
+
+# jq '. | to_entries[] | select( .key | contains("/W/"))' $1 # will only select the Watermark itemsi
+
+# jq '.[] |= . + { "Certifications": [], "Function": "", "Legacy": "" }' $1 >> tmp.json
+
+# jq '.[] | {LeadTime, Certifications}' tmp.json 
+
+# jq '."R247/N/FX"."Certifications" |= ["Watermark"]' tmp.json
+
+# jq '. | to_entries[] | select(.key | contains("/W/"))."Certifications" |= .+ ["Watermark"]' tmp.json
+
+
+# jq '. | to_entries[] | select(.key | contains("/W/")) |= .value + {"Certifications": ["Watermark"]}' tmp.json
+
+
+# jq '. | to_entries[] | select(.key | contains("/W/")) |= .value + {"Certifications": ["Watermark"]} + {"Legacy": .key}' tmp.json
+
+jq '. | to_entries[] | select(.key | contains("")) |= .value + {"Legacy": .key}' $1 >> tmp.json
+
+jq '.' tmp.json
