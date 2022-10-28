6a455c0a6f5fb4e6494024469b1c171fb1466331
shaunmsoria <shaunmsoria@gmail.com>
2022-10-28 02:25:50 +0000

    Creation of the itemDB.json file

--- OKR/ITokr/DB/iter.sh
+++ OKR/ITokr/DB/iter.sh
@@ -2,28 +2,17 @@
 
 file=$1
 
-rm -f tmp.json tmp2.json tmp3.json tmp4.json
+rm -f tmp.json tmp2.json 
 
 printf "{" >> tmp.json
 
 count=0
 
-while IFS='' read ag1; 
+jq -c -r 'keys[]' $file | while IFS='' read lSKU;
 do
-	printf "\n\t\"Key$count\":\n\t{\n\t\t\"SKU\":\"\",\n\t\t\"Description\":\"\",\n\t\t\"Supplier\":\"\",\n\t\t\"MBR\":\"\",\n\t\t\"RQ\":\"\",\n\t\t\"LeadTime\":\"\",\n\t\t\"Certifications\":\"\",\n\t\t\"Function\":\"\",\n\t\t\"MBP\":\"\",\n\t\t\"Legacy\":$ag1\n\t}," >> tmp.json
-	((count=count+1))
-
-done < <(jq -c 'keys[]' $file)
-
-sed '$ s/.$/}/' tmp.json >> tmp2.json
-
-printf "{\n" >> tmp3.json
-
-while IFS='' read value;
-do
-	lSKU=$(echo $value | jq -r '.Legacy')
 	readarray -d / -t skuArr <<< "$lSKU"
 	sku=$(printf "${skuArr[0]}")
+#	echo $sku
 	description=$(jq -r --arg v "$lSKU" '.[$v].Description' $file)
 	supplier=$(jq -r --arg v "$lSKU" '.[$v].Supplier' $file)
 	mbr=$(jq -r --arg v "$lSKU" '.[$v].MBR' $file)
@@ -36,11 +25,20 @@ do
 	arrLen=${#skuArr[@]}
 	funcTest=""
 	function=""
+	testSupplier=$(printf "${supplier[0]}")
+	mbp="0"
+	pt="0"
+	if [[ "$testSupplier" = "ROI" ]]; then
+		supplier="Roto Industries Pty Ltd"
+		function="Protection"
+	elif [[ "$testSupplier" = "Frigmac" ]]; then
+		function="Chiller"
+	fi
 	if [[ "$arrLen" != "1" ]]; then
 		funcPosition=$arrLen-1
 		funcTest=$(printf "${skuArr[$funcPosition]}")
 		if [[ "$funcTest" = "PT" ]]; then
-			function="Protection"
+			function="Protection"	
 		elif [[ "$funcTest" = "FX" ]]; then 
 			function="Fixture"
 		elif [[ "$funcTest" = "FL" ]]; then 
@@ -51,46 +49,29 @@ do
 			function="Activation"
 		elif [[ "$funcTest" = "LB" ]]; then 
 			function="LeakBlock"
-		fi	
+		fi
+	else
+#		echo $sku
+		readarray -d - -t pbaArr <<< "$sku"
+		testPBA=$(printf "${pbaArr[0]}")
+		valuePBA=$(printf "${pbaArr[1]}")
+#		echo $testPBA
+#		echo $valuePBA
+		if [[ "$testPBA" = "PBA" ]]; then
+			if [[ "$valuePBA" = "10ADG" || "$valuePBA" = "17PDGT" || "$valuePBA" = "18PDG" || "$valuePBA" = "5AVA" || "$valuePBA" = "16PVA" || "$valuePBA" = "39AHF" || "$valuePBA" = "40PSTD" || "$valuePBA" = "41PTTD" || "$valuePBA" = "42PPT" || "$valuePBA" = "43PPS" || "$valuePBA" = "44CRS" || "$valuePBA" = "17PDGT" ]]; then
+				rq="10"
+				mbp="10"
+				pt="10"
+			else
+				rq="20"
+				mbp="20"
+				pt="10"
+			fi
+		fi
 	fi
+	
+	printf "\n\t\"Key$count\":\n\t{\n\t\t\"SKU\":\"$sku\",\n\t\t\"Description\":\"$description\",\n\t\t\"Suppliers\":[\"$supplier\"],\n\t\t\"MBR\":\"$mbr\",\n\t\t\"RQ\":\"$rq\",\n\t\t\"LeadTime\":\"$leadtime\",\n\t\t\"Certifications\":[\"$certifications\"],\n\t\t\"Function\":\"$function\",\n\t\t\"MBP\":\"$mbp\",\n\t\t\"PT\":\"$pt\",\n\t\t\"Legacy\":\"$lSKU\"\n\t}," >> tmp.json
+	((count=count+1))
+done
 
-	printf "$(jq --arg leg "$lSKU" '.[] | select(.Legacy == $leg)' tmp2.json | jq --arg SKU "$sku" --arg des "$description" --arg sup "$supplier" --arg MBR "$mbr" --arg RQ "$rq" --arg lea "$leadtime" --arg cert "$certifications" --arg func "$function"  '. |= . + { "SKU": $SKU } + { "Description": $des } + { "Supplier": $sup } + { "MBR": $MBR } + { "RQ": $RQ } + { "LeadTime": $lea } + {"Certifications": [$cert] } + {"Function": $func }'),\n"  >> tmp3.json
-#	printf ",\n" >> tmp3.json
-
-
-
-done < <(jq -c '.[]' tmp2.json)
-
-
-sed '$ s/.$/}/' tmp3.json >> tmp4.json
-
-#jq '.' tmp2.json >> tmp3.json
-
-
-
-#jq -c '.' $1 | while read input;
-#do
-#
-#	echo $input | jq "keys"  >> tmp.json
-#
-##	echo $input | jq '. | to_entries[]' >> tmp.json
-##	printf "," >> tmp.json
-#	
-#
-##	printf "{\n\t\"SKU\":\"\",\n\t\"Description\":$(echo $input | jq '.[]."Description"'),\n \
-##	\"Supplier\":$(echo $input | jq '."Supplier"'),\n \
-##	\"MBR\":$(echo $input | jq '."MBR"'),\n \
-##	\"RQ\":$(echo $input | jq '."RQ"'),\n \
-##	\"LeadTime\":$(echo $input | jq '."LeadTime"'),\n \
-##	\"Legacy\": $(echo $input | jq '.' ) \
-##	},\n\
-##	" "$input" >> tmp.json
-##	cat tmp.json
-#
-#done
-
-#printf "\n}" >> tmp.json
-
-# echo $input | jq '."Description"'
-
-
+sed '$ s/.$/}/' tmp.json >> tmp2.json
