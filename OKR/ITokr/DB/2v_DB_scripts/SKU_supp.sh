9bfce0d85cd603d537e400e0c6d42e5deddf88d7
shaunmsoria <shaunmsoria@gmail.com>
2022-10-31 05:43:28 +0000

    Creating the SKU_supp.sh script

--- OKR/ITokr/DB/2v_DB_scripts/SKU_supp.sh
+++ OKR/ITokr/DB/2v_DB_scripts/SKU_supp.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
+rm -f tmp.json
+
+Inventory=$1 
+JQ_reference=$2
+
+printf "{" >> tmp.json
+
+while IFS="," read -r SKU Product Supplier Supplier_SKU Supplier_Product_Name Latest_Price Fixed_Price
+do
+	sku_test=""
+	printf "$SKU"
+	jq -r --arg sku_inv "$SKU" '.[] | select(.Legacy == $sku_inv)' $JQ_reference
+#	sku_test=$(jq -r --arg sku_inv "$SKU" '.' $JQ_reference)
+#	printf $sku_test
+#	if [[  ]];then
+#		
+#	fi
+
+#	printf "$SKU, $Product, $Supplier, $Supplier_SKU, $Supplier_Product_Name, $Latest_Price, $Fixed_Price\n\n"
+done < <(tail -n +2 $Inventory)
 
