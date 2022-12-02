593e4aceb4cf6b9e190693910299195c3037a336
shaunmsoria <shaunmsoria@gmail.com>
2022-12-02 06:43:31 +0000

    Creating new version of the json database files

--- OKR/ITokr/DB/3v_DB_scripts/dbScript.sh
+++ OKR/ITokr/DB/3v_DB_scripts/dbScript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-rm -f inv_tmp.json BOM_tmp.json
+rm -f inv_tmp.json inv_tmp2.json BOM_tmp.json BOM_tmp2.json BOM_tmp3.json BOM_tmp4.json tp1
+
+
+
 
 dbFile=$1
 
@@ -20,6 +23,8 @@ do
 	else
 		printf "\t\t\"Priority\": 0,\n" >> inv_tmp.json
 	fi
+	Supplier=$(jq -r --arg KEY "$key" '.[$KEY].Supplier' $dbFile)
+	printf "\t\t\"SupplierName\": \"$Supplier\",\n" >> inv_tmp.json
 	SupplierSKU=$(jq -r --arg KEY "$key" '.[$KEY].SupplierSKU' $dbFile)
 	printf "\t\t\"SupplierSKU\": \"$SupplierSKU\",\n" >> inv_tmp.json
 	SupplierProductName=$(jq -r --arg KEY "$key" '.[$KEY].SupplierProductName' $dbFile)
@@ -37,7 +42,7 @@ do
 	# Creation of the BOM_tmp.json file
 	
 	if [[ "$Status" == "Active" ]]; then
-		printf "\n\t{\n" >> BOM_tmp.json
+		printf "\t{\n" >> BOM_tmp.json
 		printf "\t\t\"bomSKU\": \"$sku\",\n" >> BOM_tmp.json
 		MBR=$(jq -r --arg KEY "$key" '.[$KEY].MBR' $dbFile)
 		printf "\t\t\"MBR\": $MBR,\n" >> BOM_tmp.json
@@ -58,31 +63,33 @@ do
 
 done
 
-sed '$ s/.$/]/' inv_tmp.json >> inv_tmp.json
-sed '$ s/.$/]/' BOM_tmp.json >> BOM_tmp.json
+sed '$ s/.$/\n]/' inv_tmp.json >> inv_tmp2.json
+sed '$ s/.$/]/' BOM_tmp.json >> BOM_tmp2.json
 
-printf "[\n" >> BOM_tmp2.json
+jq -c -r '.[].bomSKU' BOM_tmp2.json | sort -u >> tp1
 
-jq -c -r '.[].bomSKU' BOM_tmp.json | while IFS="" read sku;
+
+printf "[\n" >> BOM_tmp3.json
+
+#jq -c -r '.[].bomSKU' BOM_tmp2.json | while IFS="" read sku;
+for sku in $(cat tp1);
 do
-	testSKU=$(jq -r --arg SKU "$sku" '.[] | select(.bomSKU == "$SKU")' BOM_tmp.json)
-	if [[ -z "$testSKU" ]]; then
-		printf "\n\t{\n" >> BOM_tmp2.json
-		printf "\t\t\"bomSKU\": \"$sku\",\n" >> BOM_tmp2.json
-		MBR=$(jq -r --arg KEY "$key" '.[$KEY].MBR' $dbFile)
-		printf "\t\t\"MBR\": $MBR,\n" >> BOM_tmp2.json
-		Certifications=$(jq -r --arg KEY "$key" '.[$KEY].Certifications' $dbFile)
-		printf "\t\t\"Certifications\": $Certifications,\n" >> BOM_tmp2.json
-		Function=$(jq -r --arg KEY "$key" '.[$KEY].Function' $dbFile)
-		printf "\t\t\"Function\": \"$Function\",\n" >> BOM_tmp2.json
-		MBP=$(jq -r --arg KEY "$key" '.[$KEY].MBP' $dbFile)
-		printf "\t\t\"MBP\": $MBP,\n" >> BOM_tmp2.json
-		PCTM=$(jq -r --arg KEY "$key" '.[$KEY].PCTM' $dbFile)
-		printf "\t\t\"PCTM\": $PCTM,\n" >> BOM_tmp2.json
-		Legacy=$(jq -r --arg KEY "$key" '.[$KEY].Legacy' $dbFile)
-		printf "\t\t\"Legacy\": \"$Legacy\"\n" >> BOM_tmp2.json
-		printf "\t},\n" >> BOM_tmp2.json
-	fi
+ 		printf "\t{\n" >> BOM_tmp3.json
+ 		printf "\t\t\"bomSKU\": \"$sku\",\n" >> BOM_tmp3.json
+ 		MBR=$(jq -r --arg SKU "$sku" '.[] | select(.bomSKU == $SKU) | {MBR}' BOM_tmp2.json | jq '.MBR' | sort -u)
+ 		printf "\t\t\"MBR\": $MBR,\n" >> BOM_tmp3.json
+ 		Certifications=$(jq -r --arg SKU "$sku" '.[] | select(.bomSKU == $SKU) | {Certifications}' BOM_tmp2.json | jq '.Certifications' | jq '.[]' | sort -u)
+ 		printf "\t\t\"Certifications\": [$Certifications],\n" >> BOM_tmp3.json
+ 		Function=$(jq -r --arg SKU "$sku" '.[] | select(.bomSKU == $SKU) | {Function}' BOM_tmp2.json | jq '.Function' | sort -u)
+ 		printf "\t\t\"Function\": $Function,\n" >> BOM_tmp3.json
+ 		MBP=$(jq -r --arg SKU "$sku" '.[] | select(.bomSKU == $SKU) | {MBP}' BOM_tmp2.json | jq '.MBP' | sort -u)
+ 		printf "\t\t\"MBP\": $MBP,\n" >> BOM_tmp3.json
+ 		PCTM=$(jq -r --arg SKU "$sku" '.[] | select(.bomSKU == $SKU) | {PCTM}' BOM_tmp2.json | jq '.PCTM' | sort -u)
+ 		printf "\t\t\"PCTM\": $PCTM,\n" >> BOM_tmp3.json
+ 		Legacy=$(jq -r --arg SKU "$sku" '.[] | select(.bomSKU == $SKU) | {Legacy}' BOM_tmp2.json | jq '.Legacy' | sort -u)
+ 		printf "\t\t\"Legacy\": $Legacy\n" >> BOM_tmp3.json
+ 		printf "\t},\n" >> BOM_tmp3.json
+ #	fi
 done
 	
-sed '$ s/.$/]/' BOM_tmp2.json >> BOM_tmp2.json
+sed '$ s/.$/\n]/' BOM_tmp3.json >> BOM_tmp4.json
